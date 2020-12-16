a65b65dcb7edbbd8fba8f7347715821467596bdb
Ogbuji Godwin <ogbujigodwinmax@gmail.com>
2020-12-16 01:02:20 +0100

    replaced a string value

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = require('./app');
 
 
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace('PASSWORD', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, {
    useNewUrlParser: true,
