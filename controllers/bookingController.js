b26ef01af1cac6f12f36619b96e0dc6b2724051e
Ogbuji Godwin <ogbujigodwinmax@gmail.com>
2020-12-17 14:04:57 +0100

    improved stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -24,7 +24,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
            {
                name: `${tour.name} tour`,
                description: tour.summary,
-               images: [`${req.protocol}://${req.get('host')}/img/tours${tour.imageCover}`],
+               images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
                amount: tour.price * 100,
                currency: 'usd',
                quantity: 1
