d89305fd75cd262eea39c94c8023e529827f7872
Ogbuji Godwin <ogbujigodwinmax@gmail.com>
2020-12-17 13:25:24 +0100

    improved stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -19,7 +19,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
        success_url: `${req.protocol}://${req.get('host')}/my-tours`,
        cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
        customer_email: req.user.email,
-       client_reference_id: req.params.tourID,
+       client_reference_id: req.params.tourId,
        line_items: [
            {
                name: `${tour.name} tour`,
